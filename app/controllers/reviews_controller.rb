11c3bc29e3d0d7530abba0ad77df9ec2763861e9
kcchaha <kcchaha@gmail.com>
2019-06-14 21:17:39 +0000

    hide review form from visitors, allow logged in users to delete their own review after submission

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,26 @@
 class ReviewsController < ApplicationController
   def create
     @review = Product.find(params[:product_id]).reviews.new(review_params)
-
+    @review.user_id = current_user.id
+    
     if @review.save
-        redirect_to "/"
+        redirect_to "/products/#{@review.product_id}"
       else
-        redirect_to "/register"
+        redirect_to "/products/#{@review.product_id}"
     end  
   end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+ 
+    redirect_to "/products/#{@review.product_id}"
+  end
+
   private
   def review_params
     puts params
     params.require(:review).permit(:description, :rating)
   end
+
 end
